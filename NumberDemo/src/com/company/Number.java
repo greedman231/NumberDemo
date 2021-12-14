2ff2787378da016d7c8f858f21027fac59134c25
greedman231 <93518502+greedman231@users.noreply.github.com>
2021-12-14 12:01:08 +0300

    T-0
    NumberDemo

---
+++ NumberDemo/src/com/company/Number.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Number {
+    int x;
+    Number(int num){
+        x = num;
+    }
+}
---
+++ NumberDemo/src/com/company/NumberDemo.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class NumberDemo {
+
+    public static void main(String[] args) {
+        Number t1 = new Number(9);
+        Number t2 = new Number(27);
+
+        System.out.println(t1.x + "***" + t2.x);
+    }
+}
